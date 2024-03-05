c1fa91f01612ede70f5d2d65cb9f22c07434ad84
lfischy <fischbach.louis12@gmail.com>
2024-03-04 22:26:11 -0600

    updates

--- fbscraper.js
+++ fbscraper.js
@@ -7,9 +7,7 @@
 
 //TODO: change location and range based on input
 
-
 //TODO: break code down into functions
-//TODO: dynamically return amount of new listing since last refreshed - loop thru and compare .textContent of listings in array, only return the ones that are new since last refresh
 //TODO: implement proxy usage
 //FIXME: fix "requesting mainframe too early" when using stealth/headless - works fine in head mode
 //TODO: add feature to take unlimited amount of search terms + price limits, searching a diff term + price each interval
@@ -20,8 +18,10 @@ const readline = require('readline-sync');
 const hookcord = require('hookcord');
 const stealth = require("puppeteer-extra-plugin-stealth");
 
+
 puppeteer.use(stealth());
- 
+
+
 // init hookcord to send to webhook
 const Hook = new hookcord.Hook()
 Hook.login('1208975570180902932', 'QrmCiRrtQUHpz06NXNpNZ8B86clgWhWhUAp0EcRB8mXoJJewUFKyGNSWs984x5ohboj1')
@@ -49,7 +49,13 @@ browser = puppeteer
       isMobile: false,
     });
     
-    page.goto("https://www.facebook.com/", {
+    
+    
+    await login();
+
+    async function login() {
+    
+        page.goto("https://www.facebook.com/", {
     });
 
     let email = "fischbach.louis12@gmail.com"
@@ -64,53 +70,56 @@ browser = puppeteer
     await page.type('input[name="pass"]', password, {
         delay: 5,
     });
-    
-    await page.click('button[name="login"]')
 
+    await page.click('button[name="login"]') 
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
+}
+ 
+    async function storePreviousListings() {
+        let imgLocation = 'img[class="xt7dq6l xl1xv1r x6ikm8r x10wlt62 xh8yej3"]';
+        await page.waitForNavigation( {waitUntil: 'networkidle0'} );
+        
+        return page.$$eval(imgLocation, images => images.map(image => image.src) );
+    }
     
-    const baseUrl = "https://www.facebook.com/marketplace"
+    await search();
     
-    //pulls out substrings (each word)
-    searchTerm = searchTerm.split(" ")
+    
 
-    let searchUrl = "/?query="
-    let processedTerms = ""
-    for (i = 0; i < searchTerm.length; i++) {
-        processedTerms += searchTerm[i] + "%20";
+    //seperates each words and stores them in seperatedTerms - returns seperated terms
+    
+    function seperateTerms(search) {
+    
+    search = search.split(" ")
+    let seperatedTerms = " ";
+    for (i = 0; i < search.length; i++) {
+        seperatedTerms += search[i] + "%20";
+    }
+    return seperatedTerms;
     }
 
-
-    // make inital search
-    await page.waitForNavigation( {waitUntil: 'networkidle0'} )
-    await page.goto(baseUrl + searchUrl + processedTerms, {
-        waitUntil: "networkidle0",
-    });
-
- 
-    //sets price limits
-    // await page.waitForNavigation({ waitUntil: 'networkidle0' })
-
-    let currentUrl = baseUrl + "/search?" + "minPrice=" + lowerLimit + "&maxPrice=" + upperLimit + "&query=" + processedTerms 
-    await page.goto(currentUrl, {
-        waitUntil: "networkidle0",
-    });
-
-
-        
-    console.log( "bp 1 " + currentUrl)
-
+    // search, with filters applied
+    async function search() {
     
-    //sorts listings by newest to oldest
-    await page.goto(currentUrl + "&sortBy=creation_time_descend&exact=false", {
+        let searchUrl = seperateTerms(searchTerm);
+    await page.goto("https://www.facebook.com/marketplace/search?query=" + searchUrl + "&minPrice=" + lowerLimit + "&maxPrice=" + upperLimit + "&sortBy=creation_time_descend&exact=false", {
         waitUntil: "networkidle0",
     });
 
-    //reloads page x # of ms and returns new content to output
-        
-    console.log("bp 2 " + currentUrl)
-
-    await refresh()
+    }
     
+    //( async () => {
+    //    let previousListings = await storePreviousListings();
+    //    console.log(previousListings);
+    //})();
+    
+    let oldListings = {
+        titles: [],
+        prices: [],
+        locations: [],
+        images: [],
+        links: []
+    };
     // refresh() reloads page and wait for elements to load in, stores data in an array
     async function refresh() {
         
@@ -119,170 +128,90 @@ browser = puppeteer
             await page.reload();
             //await page.waitForNavigation({waitUntil: 'networkidle0'})
     
-            const priceSpan = 'span[class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u"]'
-            // stores textcontent of above <span> in an array
-            let arrPrices = await page.$$eval(priceSpan, prices => prices.map(price => price.textContent) )
-
-            const titleSpan = 'span[class="x1lliihq x6ikm8r x10wlt62 x1n2onr6"]' 
-            // stores textcontent of above <span> in an array
-            let arrTitles = await page.$$eval(titleSpan, titles => titles.map(title => title.textContent) )
-    
-            const locationSpan = 'span[class="x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft x1j85h84"]'
-            // stores textcontent of above <span> in an array
-            let arrLocations = await page.$$eval(locationSpan, locations => locations.map(location => location.textContent) )
-    
-            const linkLocation = 'a[class="x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lku1pv"]'
-            // stores href of above <a> in an array
-            const arrLinks = await page.$$eval(linkLocation, links => links.map(link => link.href) )
-
-            const imageLocation = 'img[class="xt7dq6l xl1xv1r x6ikm8r x10wlt62 xh8yej3"]'
-            // stores src of above <img> in an array
-            const arrImages = await page.$$eval(imageLocation, images => images.map(image => image.src) )
-        
-            const arrays = [arrTitles, arrPrices, arrLocations, arrImages, arrLinks];
-
-            //console.log(arrays)
-
-            //const message = "Listings:\n" + arrTitles[0] + "\n" + arrPrices[0] + "\n" + arrLocations[0] + "\n" + arrImages[0] + "\n" + arrLinks[0] + "\n" + arrTitles[1] + "\n" + arrPrices[1] + "\n" + arrLocations[1] + "\n" + arrImages[1] + "\n" + arrLinks[1] + "\n" + arrTitles[2] + "\n" + arrPrices[2] + "\n" + arrLocations[2] + "\n" + arrImages[2] + "\n" + arrLinks[2] + "\n_________________________________________________________________________________________________________________________________________________________________________________________________"
-
-            let title0 = arrTitles[0]
-            let price0 = arrPrices[0]
-            let location0 = arrLocations[0]
-            let image0 = arrImages[0]
-            let link0 = arrLinks[0]
-
-            let title1 = arrTitles[1]
-            let price1 = arrPrices[1]
-            let location1 = arrLocations[1]
-            let image1 = arrImages[1]
-            let link1 = arrLinks[1]
-
-            let title2 = arrTitles[2]
-            let price2 = arrPrices[2]
-            let location2 = arrLocations[2]
-            let image2 = arrImages[2]
-            let link2 = arrLinks[2]
-                  
-            Hook.setPayload({
-                "embeds": [{
-                "title": "Link to listing",
-                "color": 15257231,
-                "fields": [
-                    {
-                        
-                        "name": " ",
-                        "value": `${title0}`,    
-                    },
-                    {
-                        "name": " ",
-                        "value": `${price0}`,
-                    },
-                    {
-                        "name": " ",
-                        "value": `${location0}`,
-                    }
-
-
-                ],
-
-                
-                "url": `${link0}`, 
-                
-                
-                "image": {
-                    "url": `${image0}`
-                }
-                }]
-            }).fire();  
+            const selectors = {
+                priceSpan: 'span[class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u"]',
+                titleSpan: 'span[class="x1lliihq x6ikm8r x10wlt62 x1n2onr6"]',
+                locationSpan: 'span[class="x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft x1j85h84"]',
+                linkLocation: 'a[class="x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lku1pv"]',
+                imageLocation: 'img[class="xt7dq6l xl1xv1r x6ikm8r x10wlt62 xh8yej3"]'
+            };
             
-            Hook.setPayload({
-                "embeds": [{
-                "title": "Link to listing",
-                "color": 15257231,
-                "fields": [
-                    {
-                        
-                        "name": " ",
-                        "value": `${title1}`,    
-                    },
-                    {
-                        "name": " ",
-                        "value": `${price1}`,
-                    },
-                    {
-                        "name": " ",
-                        "value": `${location1}`,
-                    }
+            let currentListings = {
+                titles: await page.$$eval(selectors.titleSpan, titles => titles.map(title => title.textContent) ),
+                prices: await page.$$eval(selectors.priceSpan, prices => prices.map(price => price.textContent) ),
+                locations: await page.$$eval(selectors.locationSpan, locations => locations.map(location => location.textContent) ),
+                links: await page.$$eval(selectors.linkLocation, links => links.map(link => link.href) ),
+                images: await page.$$eval(selectors.imageLocation, images => images.map(image => image.src) )
 
-
-                ],
-
-                
-                "url": `${link1}`, 
-                
-                
-                "image": {
-                    "url": `${image1}`
+            }
+            
+            //let newestListings = lodash.difference(currentListings.images, oldListings.images);
+            
+            let imageIndices = [];
+            currentListings.images.forEach((image, i) => {
+                if (!oldListings.images.includes(image)) {
+                    imageIndices.push(i);
                 }
-                }]
-            }).fire();
+            });
 
-            Hook.setPayload({
-                "embeds": [{
-                "title": "Link to listing",
-                "color": 15257231,
-                "fields": [
-                    {
-                        
-                        "name": " ",
-                        "value": `${title2}`,    
-                    },
-                    {
-                        "name": " ",
-                        "value": `${price2}`,
-                    },
-                    {
-                        "name": " ",
-                        "value": `${location2}`,
-                    }
+            let newestListings = {
+                titles: imageIndices.map(i => currentListings.titles[i]),
+                prices: imageIndices.map(i => currentListings.prices[i]),
+                locations: imageIndices.map(i => currentListings.locations[i]),
+                links: imageIndices.map(i => currentListings.images[i]),
+                images: imageIndices.map(i => currentListings.links[i])
+            }
 
-
-                ],
-
-                
-                "url": `${link2}`, 
-                
-                
-                "image": {
-                    "url": `${image2}`
-                }
-                }]
-            }).fire();
+            oldListings = currentListings;
+        
 
             let min = 3600000; // 60 min
             let max = 4500000; // 75 min 
+            
+            //let max = 10000;
+            //let min = 20000;
 
             let randMs = Math.floor(Math.random() * (max - min + 1)) + min;
             let msToMins = randMs / 60000;
             console.log("Calling refresh again in " + msToMins + " minutes");
-
-            Hook.setPayload({
-                "embeds": [{
-                "title": "New results in:",
-                "color": 15257231,
-                "fields": [
-                    {
-                        "name": " Minutes:",
-                        "value": `${msToMins}`,
-                        
-                    }
-                ]
-                }]
-            }).fire();
         
             setTimeout(refresh, randMs);
+
+            for (let i = 0; i < newestListings.images.length; ++i) {
+                
+                Hook.setPayload({
+                    "embeds": [{
+                    "title": "Link to listing",
+                    "color": 15257231,
+                    "fields": [
+                        {
+                            "name": " ",
+                            "value": `${newestListings.titles[i]}`,
+                        },
+                        {
+                            "name": " ",
+                            "value": `${newestListings.prices[i]}`
+                        },
+                        {
+                            "name": " ",
+                            "value": `${newestListings.locations[i]}`
+                        },
+                    ],
+
+                    "url": `${newestListings.images[i]}`,
+
+                    "image": {
+                        "url": `${newestListings.links[i]}`
+                    }
+                    }]
+                }).fire();
+                // sets delay to avoid rate limit - only necessary on the first call to refresh
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            }
     }
 
+    await refresh();
+    
 })
   .catch((error) => {
     console.log(error);
