07a21b3a7e5207814a8dbb50c18eed6f54f555dd
lfischy <fischbach.louis@gmail.com>
2024-03-21 12:25:44 -0500

    revised radius and location settings

--- js/fbscraper.js
+++ js/fbscraper.js
@@ -1,39 +1,36 @@
-//Things to do, in no particular order:
-
-
-
 //TODO: ban protection, refresh marketplace random number of times - switch between refreshing page and navigating to new page. Throw in random searches w/ diff filters etc - (dont return random search results)
 //TODO: return "listed x num of hrs ago"
 
 //TODO: change location and range based on input
 
-//TODO: break code down into functions
 //TODO: implement proxy usage
-//FIXME: fix "requesting mainframe too early" when using stealth/headless - works fine in head mode
-//TODO: add feature to take unlimited amount of search terms + price limits, searching a diff term + price each interval
+//FIXME: fix "requesting mainframe too early" when using stealth/headless - works fine in head mode ??
 
+// NO NEED TO LOGIN. Can search w/o account  - add functionality to get rid of fb pop up
+// add auto messager - can only be used with an acct
+//dont spit back all listings to user on first refresh call - use count variable w/in refresh
+// look into diff results, acct vs no acct
 
 const puppeteer = require("puppeteer-extra");
 const readline = require('readline-sync'); 
 const hookcord = require('hookcord');
 const stealth = require("puppeteer-extra-plugin-stealth");
-
+const { Keyboard } = require("puppeteer");
 
 puppeteer.use(stealth());
-
 
 // init hookcord to send to webhook
 const Hook = new hookcord.Hook()
 Hook.login('1208975570180902932', 'QrmCiRrtQUHpz06NXNpNZ8B86clgWhWhUAp0EcRB8mXoJJewUFKyGNSWs984x5ohboj1')
 
 console.log(" ")
-console.log("Fill out the following questions to narrow your search")
 let searchTerm = readline.question("Enter search term: ");
 const lowerLimit = readline.question("Enter low price: ")
 const upperLimit = readline.question("Enter high price: ")
+const location = readline.question("Enter search location: (City, state) ");
+const radius = readline.question("Enter radius: (Choose 1-11) 1(1mi) 2(2mi) 3(5mi) 4(10mi) 5(20mi) 6(40) 7(60mi) 8(80mi) 9(100mi) 10(250mi) 11(500mi) ");
 console.log(" ")
 console.log(" ")
-
 
 let page = null;
 let browser = null;
@@ -44,49 +41,12 @@ browser = puppeteer
     page = await browser.newPage();
     page.setDefaultNavigationTimeout(0);
     page.setViewport({
-      width: 1280,
-      height: 800,
+      width: 1920,
+      height: 1280,
       isMobile: false,
     });
-    
-    
-    
-    await login();
-
-    async function login() {
-    
-        page.goto("https://www.facebook.com/", {
-    });
-
-    let email = "fischbach.louis12@gmail.com"
-    let password = "Louis123321!"
-    
-    await page.waitForSelector('input[name="email"]');
-
-    await page.type('input[name="email"]', email, {
-        delay: 5,
-      });
-
-    await page.type('input[name="pass"]', password, {
-        delay: 5,
-    });
-
-    await page.click('button[name="login"]') 
-    await page.waitForNavigation({waitUntil: 'networkidle0'});
-}
- 
-    async function storePreviousListings() {
-        let imgLocation = 'img[class="xt7dq6l xl1xv1r x6ikm8r x10wlt62 xh8yej3"]';
-        await page.waitForNavigation( {waitUntil: 'networkidle0'} );
-        
-        return page.$$eval(imgLocation, images => images.map(image => image.src) );
-    }
-    
-    await search();
-    
-    
-
-    //seperates each words and stores them in seperatedTerms - returns seperated terms
+     
+    //seperates each word and stores them in seperatedTerms - returns seperated terms
     
     function seperateTerms(search) {
     
@@ -97,21 +57,92 @@ browser = puppeteer
     }
     return seperatedTerms;
     }
+  
+    async function setLocation() {
+        console.log("Setting location...");
+        
+        // close login popup
+        
+        await page.waitForSelector('input[class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]');
+        
+        await page.click('input[class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]');
+        await page.keyboard.press('Escape');
+        
+        /*
+        // close zip code pop up
+        await page.waitForSelector('input[class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1yc453h xhb22t3 xb5gni xcj1dhv x2s2ed0 xq33zhf xjyslct xjbqb8w xnwf7zb x40j3uw x1s7lred x15gyhx8 x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xzsf02u xdl72j9 x1iyjqo2 xs83m0k xjb2p0i x6prxxf xeuugli x1a2a7pz xm7lytj xn6708d xdvlbce x1ye3gou x1n2onr6 x15h3p50 xc9qbxq"]');
+        await page.keyboard.press('Tab');
+        await page.keyboard.press('Enter');
+        */
+        
+        //open change location window
+        await page.waitForSelector('div[class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x78zum5 x1a2a7pz x1xmf6yo"]');
+        await page.click('div[class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x78zum5 x1a2a7pz x1xmf6yo"]');
+        await page.click('div[class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x78zum5 x1a2a7pz x1xmf6yo"]');
+
+        //set location
+        await page.waitForSelector('input[class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]');
+        await page.click('input[class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]');
+        await page.keyboard.type(location);
+
+        await page.waitForSelector('span[class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x676frb x1nxh6w3 x1sibtaa xo1l8bm xi81zsa x1yc453h"]');
+        await page.keyboard.press('ArrowDown'); 
+        await page.keyboard.press('Enter'); 
+
+        //set radius
+        await page.waitForSelector('div[class="xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xzsf02u x78zum5 x1jchvi3 x1fcty0u x132q4wb xdj266r x11i5rnm xat24cr x1mh8g0r x1a2a7pz x9desvi x1pi30zi x1a8lsjc x1swvt13 x1n2onr6 x16tdsg8 xh8yej3 x1ja2u2z"]');
+        await page.click('div[class="xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xzsf02u x78zum5 x1jchvi3 x1fcty0u x132q4wb xdj266r x11i5rnm xat24cr x1mh8g0r x1a2a7pz x9desvi x1pi30zi x1a8lsjc x1swvt13 x1n2onr6 x16tdsg8 xh8yej3 x1ja2u2z"]');
+
+        //implement variable to choose radius
+        for (let i = 0; i < 10; ++i) {
+            await page.keyboard.press('ArrowDown'); 
+ 
+        }
+
+        await page.keyboard.press('Enter');
+        
+        await page.waitForSelector('div[class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1r1pt67"]');
+        await page.click('div[class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1r1pt67"]');
+        console.log("Location set!");
+    }
+    
+    async function setRadius() {
+        
+        console.log("resetting radius....")
+
+        await page.waitForSelector('div[class=""xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xzsf02u x78zum5 x1jchvi3 x1fcty0u x132q4wb xdj266r x11i5rnm xat24cr x1mh8g0r x1a2a7pz x9desvi x1pi30zi x1a8lsjc x1swvt13 x1n2onr6 x16tdsg8 xh8yej3 x1ja2u2z""]');
+        await page.click('div[class="xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xzsf02u x78zum5 x1jchvi3 x1fcty0u x132q4wb xdj266r x11i5rnm xat24cr x1mh8g0r x1a2a7pz x9desvi x1pi30zi x1a8lsjc x1swvt13 x1n2onr6 x16tdsg8 xh8yej3 x1ja2u2z"]');
+
+        for (let i = 0; i < 10; ++i) {
+            await page.keyboard.press('ArrowDown'); 
+        }
+        
+        // 1 sec delay
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        await page.keyboard.press('Enter');
+
+        // 1 sec delay
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // waits on apply button, and presses it
+        await page.waitForSelector('div[class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1r1pt67"]');
+        await page.click('div[class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1r1pt67"]');
+
+        console.log("radius reset") 
+
+    }
 
     // search, with filters applied
     async function search() {
-    
+        console.log("Searching...");
         let searchUrl = seperateTerms(searchTerm);
-    await page.goto("https://www.facebook.com/marketplace/search?query=" + searchUrl + "&minPrice=" + lowerLimit + "&maxPrice=" + upperLimit + "&sortBy=creation_time_descend&exact=false", {
-        waitUntil: "networkidle0",
-    });
+        await page.goto("https://www.facebook.com/marketplace/search?query=" + searchUrl + "&minPrice=" + lowerLimit + "&maxPrice=" + upperLimit + "&sortBy=creation_time_descend&exact=false", {
+            waitUntil: "networkidle0",
+        });
+        console.log("Search completed");
 
     }
-    
-    //( async () => {
-    //    let previousListings = await storePreviousListings();
-    //    console.log(previousListings);
-    //})();
     
     let oldListings = {
         titles: [],
@@ -120,14 +151,34 @@ browser = puppeteer
         images: [],
         links: []
     };
+    
+    await search();
+    await setLocation();
+    await refresh();
+
     // refresh() reloads page and wait for elements to load in, stores data in an array
     async function refresh() {
         
             console.log("Started refresh");
-        
+            // let count = 0;
+
+            //delays reloading by 2 seconds - prevents location changing to san fran
+            await new Promise(resolve => setTimeout(resolve, 4000));
             await page.reload();
-            //await page.waitForNavigation({waitUntil: 'networkidle0'})
-    
+            //setting delay to avoid mainframe error after refreshing
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            //closes login popup
+            await page.waitForSelector('div[class="x1egiwwb x4l50q0"]');
+            await page.click('div[class="x1egiwwb x4l50q0"]');
+            await page.keyboard.press('Escape');
+
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            await setRadius();
+
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
             const selectors = {
                 priceSpan: 'span[class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u"]',
                 titleSpan: 'span[class="x1lliihq x6ikm8r x10wlt62 x1n2onr6"]',
@@ -144,12 +195,11 @@ browser = puppeteer
                 images: await page.$$eval(selectors.imageLocation, images => images.map(image => image.src) )
 
             }
-            
-            //let newestListings = lodash.difference(currentListings.images, oldListings.images);
-            
+                        
             let imageIndices = [];
-            currentListings.images.forEach((image, i) => {
-                if (!oldListings.images.includes(image)) {
+
+            currentListings.titles.forEach((title, i) => {
+                if (!oldListings.titles.includes(title)) {
                     imageIndices.push(i);
                 }
             });
@@ -164,15 +214,21 @@ browser = puppeteer
 
             oldListings = currentListings;
         
-
-            let min = 3600000; // 60 min
-            let max = 4500000; // 75 min 
+            //let min = 3600000; // 60 min
+            //let max = 4500000; // 75 min 
             
-            //let max = 10000;
-            //let min = 20000;
+            let max = 240000; // 4 min
+            let min = 381000; // 6.35 min
 
             let randMs = Math.floor(Math.random() * (max - min + 1)) + min;
             let msToMins = randMs / 60000;
+            
+            /*
+            if (count < 1) {
+                console.log(" ");
+                return;
+            }
+            */
             console.log("Calling refresh again in " + msToMins + " minutes");
         
             setTimeout(refresh, randMs);
@@ -205,12 +261,13 @@ browser = puppeteer
                     }
                     }]
                 }).fire();
+                
                 // sets delay to avoid rate limit - only necessary on the first call to refresh
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }
-    }
+                await new Promise(resolve => setTimeout(resolve, 1500));
 
-    await refresh();
+            }
+            // count += 1;
+    }
     
 })
   .catch((error) => {
